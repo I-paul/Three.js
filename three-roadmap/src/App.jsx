7fb69d56a26435cb4bf8d8a2df0ef306da9efdab
Israel Paul <130955174+I-paul@users.noreply.github.com>
2025-04-12 14:29:37 +0530

    level 2
    antialiasing, groups and vector3

--- three-roadmap/src/App.jsx
+++ three-roadmap/src/App.jsx
@@ -8,20 +8,31 @@ const App = () => {
   useEffect(() => {
     const scene = new THREE.Scene();
     const cbgeometry = new THREE.BoxGeometry(1, 1, 1);
-    const cbmaterial = new THREE.MeshBasicMaterial({ color: "red" });
+    const cbmaterial = new THREE.MeshBasicMaterial({ color: "red"});
 
     const cbmesh = new THREE.Mesh(cbgeometry, cbmaterial);
-    scene.add(cbmesh);
+    const cbmesh2 = new THREE.Mesh(cbgeometry, cbmaterial);
+    cbmesh2.position.x = 2;
+    cbmesh2.position.y = -2;
+    const cbmesh3 = new THREE.Mesh(cbgeometry, cbmaterial);
+    cbmesh3.position.x = -2;
 
+    const grp = new THREE.Group();
+    grp.add(cbmesh);
+    grp.add(cbmesh2);
+    grp.add(cbmesh3);
+    grp.position.y= 2;
+    scene.add(grp);
+    const axesHelper = new THREE.AxesHelper(5);
+    scene.add(axesHelper);
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.z = 5;
 
-    const renderer = new THREE.WebGLRenderer({ canvas: canvasRef.current });
+    const renderer = new THREE.WebGLRenderer({ canvas: canvasRef.current, antialias: true });
     renderer.setSize(window.innerWidth, window.innerHeight);
 
     const controls = new OrbitControls(camera, renderer.domElement);
     controls.enableDamping = true;
-    controls.autoRotate = true;
     window.addEventListener("resize", () => {
       const aspect = window.innerWidth / window.innerHeight;
       camera.aspect = aspect;
